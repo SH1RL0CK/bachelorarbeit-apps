25d6f8cc710671768cb1ac1e5e680ccc89b1fedf
SH1RL0CK <50455686+SH1RL0CK@users.noreply.github.com>
2025-07-19 22:04:44 +0200

    Add semantics support for web platform

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
 
 import 'package:flutter_app/cine_lines_app.dart';
 
 void main() {
   runApp(const CineLinesApp());
+  if (kIsWeb) {
+    SemanticsBinding.instance.ensureSemantics();
+  }
 }
